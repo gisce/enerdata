a491a04fbcb16b43e758ab3e09751f5ccafcb34c
Eduard Carreras <ecarreras@gisce.net>
2014-12-30 14:24:50 +0100

    Range of hours per periods

--- enerdata/contracts/tariff.py
+++ enerdata/contracts/tariff.py
@@ -1,8 +1,46 @@
+def check_range_hours(hours):
+    before = (0, 0)
+    for range_hours in sorted(hours):
+        start, end = range_hours
+        if start >= end:
+            return False
+        if start < 0 or start > 24:
+            return False
+        if end < 1 or end > 24:
+            return False
+        if start < before[1]:
+            return False
+        before = (start, end)
+    return True
+
+
 class Tariff(object):
     """Energy DSO Tariff.
     """
-    def __init__(self, code, **kwargs):
-        self.periods = set()
+    def __init__(self, code):
+        self.code = code
+        self._periods = tuple()
+
+    @property
+    def periods(self):
+        return self._periods
+
+    @periods.setter
+    def periods(self, value):
+        self._periods = value
+        hours = 0
+        for period in self.energy_periods.values():
+            hours += period.total_summer_hours
+        if hours != 24:
+            raise ValueError('The range of hours must be 24h')
+
+    @property
+    def energy_periods(self):
+        return dict((p.code, p) for p in self.periods if p.type == 'te')
+
+    @property
+    def power_periods(self):
+        return dict((p.code, p) for p in self.periods if p.type == 'tp')
 
     def get_number_of_periods(self):
         return len([p for p in self.periods if p.type == 'te'])
@@ -11,10 +49,32 @@ class Tariff(object):
 class TariffPeriod(object):
     """Tariff period.
     """
-    def __init__(self, code, ptype):
+    def __init__(self, code, ptype, **kwargs):
         assert ptype in ('te', 'tp')
         self.code = code
         self.type = ptype
+        winter_hours = kwargs.pop('winter_hours', [(0, 24)])
+        if not check_range_hours(winter_hours):
+            raise ValueError('Invalid winter hours')
+        self.winter_hours = winter_hours
+        summer_hours = kwargs.pop('summer_hours', [(0, 24)])
+        if not check_range_hours(summer_hours):
+            raise ValueError('Invalid summer hours')
+        self.summer_hours = summer_hours
+
+    @property
+    def total_summer_hours(self):
+        hours = 0
+        for range_h in self.summer_hours:
+            hours += (range_h[1] - range_h[0])
+        return hours
+
+    @property
+    def total_winter_hours(self):
+        hours = 0
+        for range_h in self.winter_hours:
+            hours += (range_h[1] - range_h[0])
+        return hours
 
 
 class T20A(Tariff):
@@ -28,4 +88,13 @@ class T20A(Tariff):
         self.min_power = 0
         self.max_power = 10
         self.type = 'BT'
+
+class T20DHA(Tariff):
+    def __init__(self):
+        self.code = '2.0DHA'
+        self.periods = (
+            TariffPeriod('P1', 'te', winter_hours=[(12, 22)], summer_hours=[(13, 23)]),
+            TariffPeriod('P2', 'te', winter_hours=[(0, 12), (22, 24)], summer_hours=[(0, 13), (23, 24)])
+        )
+
 
--- spec/contracts/tariff_spec.py
+++ spec/contracts/tariff_spec.py
@@ -1,5 +1,5 @@
 from expects.testing import failure
-
+from expects import *
 from enerdata.contracts.tariff import *
 
 with description('Create a period'):
@@ -10,13 +10,68 @@ with description('Create a period'):
     with it('fails when is not "te" nor "tp"'):
         with failure:
             TariffPeriod('P1', 'foo')
+    with it('should raise an exception if range of hours is invalid'):
+        expect(lambda: TariffPeriod('P1', 'te', winter_hours=[
+            (0, 12), (11, 23)
+        ])).to(raise_error(ValueError, 'Invalid winter hours'))
+        expect(lambda: TariffPeriod('P1', 'te', summer_hours=[
+            (0, 12), (11, 23)
+        ])).to(raise_error(ValueError, 'Invalid summer hours'))
+
+with description('A period'):
+    with it('should have a range of hours 24 for winter by default'):
+        p1 = TariffPeriod('P1', 'te')
+        assert p1.winter_hours == [(0, 24)]
+    with it('should be possible to set the range of winter hours in creation'):
+        p1 = TariffPeriod('P1', 'te', winter_hours=[(0, 12)])
+        assert p1.winter_hours == [(0, 12)]
+    with it('should have a range of hours 24 for summer by default'):
+        p1 = TariffPeriod('P1', 'te')
+        assert p1.summer_hours == [(0, 24)]
+    with it('should be possible to set the range of sumer hours in creation'):
+        p1 = TariffPeriod('P1', 'te', summer_hours=[(0, 12)])
+        assert p1.summer_hours == [(0, 12)]
+    with it('has to validate range hours is correct'):
+        assert check_range_hours([(0, 12)]) is True
+        assert check_range_hours([(0, 12), (12, 24)]) is True
+        assert check_range_hours([(-1, 0)]) is False
+        assert check_range_hours([(0, 25)]) is False
+        assert check_range_hours([(0, 0)]) is False
+        assert check_range_hours([(4, 1)]) is False
+        assert check_range_hours([(0, 1), (0, 2)]) is False
+        assert check_range_hours([(0, 12), (12, 24)]) is True
+    with it('should know the total of hours in summer'):
+        p1 = TariffPeriod('P1', 'te', summer_hours=[(0, 12), (22, 24)])
+        assert p1.total_summer_hours == 14
+    with it('should know the total of hours in winter'):
+        p1 = TariffPeriod('P1', 'te', winter_hours=[(12, 22)])
+        assert p1.total_winter_hours == 10
+
 
 
 with context('A tariff'):
     with before.all:
         self.tariff = Tariff('T1')
-    with it('periods should be a set type'):
-        assert isinstance(self.tariff.periods, set)
+    with it('periods should be a tuple type'):
+        assert isinstance(self.tariff.periods, tuple)
     with it('should return the number of periods of te'):
-        self.tariff.periods = (TariffPeriod('1', 'te'), TariffPeriod('2', 'te'))
+        self.tariff.periods = (
+            TariffPeriod('1', 'te', summer_hours=[(12, 22)]),
+            TariffPeriod('2', 'te', summer_hours=[(0, 12), (22, 24)])
+        )
         assert self.tariff.get_number_of_periods() == 2
+    with it('should return the periods of energy'):
+        assert len(self.tariff.energy_periods) == 2
+        assert self.tariff.energy_periods.keys() == ['1', '2']
+    with it('should return the periods of power'):
+        assert len(self.tariff.power_periods) == 0
+        self.tariff.periods += (TariffPeriod('1', 'tp'),)
+        assert len(self.tariff.power_periods) == 1
+        assert self.tariff.power_periods.keys() == ['1']
+    with it('should have 24h of range ours in its energy periods'):
+        def set_periods():
+            self.tariff.periods = (
+                TariffPeriod('1', 'te', summer_hours=[(12, 22)]),
+                TariffPeriod('2', 'te', summer_hours=[(0, 12), (22, 23)])
+            )
+        expect(set_periods).to(raise_error(ValueError, 'The range of hours must be 24h'))
