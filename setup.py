8160c6d0be4e60936fc5c9f3e135f174d8f60ec2
Eduard Carreras <ecarreras@gisce.net>
2015-08-25 12:02:15 +0200

    Bump to v0.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='enerdata',
-    version='0.2.0',
+    version='0.3.0',
     packages=find_packages(),
     url='http://code.gisce.net',
     license='MIT',
