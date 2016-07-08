5c1ee0b628e69d34a45d14d46f10574d86822565
Xavi Torelló <info@xaviertorello.cat>
2016-07-08 12:18:59 +0200

    py2-py3 compat :: Profile Dragger round solved
    Py3 rounds following 'Round to nearest, ties to even'
     – rounds to the nearest value; if the number falls midway it is rounded to the nearest value with an even (zero) least significant bit, which occurs 50% of the time; this is the default for binary floating-point and the recommended default for decimal.
    
    Solved using Decimal lib and ensuring rounding with:
    
    if >= .5
        get next integer
    else
        get previous one
    
    4.5 -> 5
    4.6 -> 5
    4 -> 4
    4.2 -> 4
    4.49 -> 4

--- enerdata/profiles/__init__.py
+++ enerdata/profiles/__init__.py
@@ -2,7 +2,8 @@ try:
     from collections import Counter
 except ImportError:
     from backport_collections import Counter
-from decimal import Decimal
+
+from decimal import Decimal, ROUND_HALF_UP
 
 
 class Dragger(Counter):
@@ -14,5 +15,8 @@ class Dragger(Counter):
 
         number = Decimal(str(number)) + self[key]
         aprox = int(round(number))
+
+        aprox = int( number.quantize(Decimal('1'), rounding=ROUND_HALF_UP) )
+
         self[key] = number - aprox
-        return aprox
+        return aprox  + 0
