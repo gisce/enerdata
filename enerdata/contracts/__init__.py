bea021afb2d77808296bdd6ad7f409deddc78f16
Xavi Torelló <info@xaviertorello.cat>
2016-07-08 10:20:32 +0200

    py2-py3 compat -> future imports (absolute path, print, unicode, ..)

--- enerdata/contracts/__init__.py
+++ enerdata/contracts/__init__.py
@@ -1,2 +1,6 @@
-from contract import *
-from tariff import *
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+from future import *
+
+from .contract import *
+from .tariff import *
