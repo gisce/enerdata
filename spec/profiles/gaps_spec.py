68774d44513c2e4eb14a36cf0e2b44fb09e6e9d4
Xavi Torelló <info@xaviertorello.cat>
2016-07-12 14:16:05 +0200

    Solved bug commiting 25eb5e1
    spec/profiles/gaps_spec.py still the expected one

--- spec/profiles/gaps_spec.py
+++ spec/profiles/gaps_spec.py
@@ -85,3 +85,201 @@ with description('A profile with gaps'):
         expect(profile_estimated.total_consumption).to(equal(total_energy))
 
 
+    with context('Is an empty profile'):
+        with it('has to generate all the profile estimating'):
+            balance = Counter()
+            tariff = T20DHA()
+            tariff.cof = 'A'
+            profile = Profile(
+                self.profile.start_date, self.profile.end_date, []
+            )
+            for ph in self.complete_profile:
+                period = tariff.get_period_by_date(ph.date)
+                balance[period.code] += ph.measure
+            with vcr.use_cassette('spec/fixtures/ree/201503-201504.yaml'):
+                profile_estimated = profile.estimate(tariff, balance)
+
+            total_energy = sum(balance.values())
+            expect(profile_estimated.total_consumption).to(equal(total_energy))
+
+        with it('has to generate all the profile estimating if all measures are invalid'):
+
+            balance = Counter()
+            tariff = T20DHA()
+            tariff.cof = 'A'
+
+            measures = []
+            for ph in self.complete_profile:
+                period = tariff.get_period_by_date(ph.date)
+                balance[period.code] += ph.measure
+                measures.append(ProfileHour(ph.date, ph.measure, False))
+
+            profile = Profile(
+                self.profile.start_date, self.profile.end_date, measures
+            )
+
+            with vcr.use_cassette('spec/fixtures/ree/201503-201504.yaml'):
+                profile_estimated = profile.estimate(tariff, balance)
+
+            total_energy = sum(balance.values())
+            expect(profile_estimated.total_consumption).to(equal(total_energy))
+
+    with context('If the balance is less than profile'):
+        with it('has to fill with 0 the gaps'):
+            balance = Counter()
+            tariff = T20DHA()
+            tariff.cof = 'A'
+
+            balance = self.profile.get_hours_per_period(tariff, only_valid=True)
+            for period in balance:
+                balance[period] -= 10
+            with vcr.use_cassette('spec/fixtures/ree/201503-201504.yaml'):
+                profile_estimated = self.profile.estimate(tariff, balance)
+            expect(profile_estimated.n_hours).to(equal(len(self.complete_profile)))
+
+            for gap in self.profile.gaps:
+                pos = bisect.bisect_left(
+                    profile_estimated.measures,
+                    ProfileHour(gap, 0, True)
+                )
+                measure = profile_estimated.measures[pos]
+                expect(measure.measure).to(equal(0))
+
+            total_energy = sum(balance.values())
+            # Adjust
+            profile_estimated = profile_estimated.adjust(tariff, balance)
+            expect(profile_estimated.total_consumption).to(equal(total_energy))
+
+    with it('must fail adjusting'):
+        balance = Counter()
+        tariff = T20DHA()
+        for ph in self.profile.measures:
+            period = tariff.get_period_by_date(ph.date)
+            balance[period.code] += ph.measure
+        expect(len(self.profile.gaps)).to(be_above(0))
+
+        def adjust_error():
+            self.profile.adjust(tariff, balance)
+
+        expect(adjust_error).to(raise_error(Exception, 'Is not possible to adjust a profile with gaps'))
+
+
+
+with description('A complete profile with different energy than balance'):
+    with before.all:
+        import random
+        measures = []
+        start = TIMEZONE.localize(datetime(2015, 3, 1, 1))
+        end = TIMEZONE.localize(datetime(2015, 4, 1, 0))
+
+        gap_start = TIMEZONE.localize(datetime(2015, 3, 15))
+        gap_end = TIMEZONE.localize(datetime(2015, 3, 16))
+        start_idx = start
+        self.gaps = []
+        while start_idx <= end:
+            energy = random.randint(0, 10)
+            measures.append(ProfileHour(
+                TIMEZONE.normalize(start_idx), energy, True
+            ))
+            start_idx += timedelta(hours=1)
+        self.profile = Profile(start, end, measures)
+
+    with it('must not have gaps'):
+        complete_hours = Counter()
+        tariff = T20DHA()
+
+        # There is no gaps
+        expect(self.profile.gaps).to(contain_exactly(*self.gaps))
+
+        profile_hours = self.profile.get_hours_per_period(
+            tariff, only_valid=True
+        )
+
+        for ph in self.profile.measures:
+            period = tariff.get_period_by_date(ph.date)
+            complete_hours[period.code] += 1
+
+        for period in complete_hours:
+            expect(profile_hours[period]).to(equal(complete_hours[period]))
+
+    with it('must not to estimate'):
+        tariff = T20DHA()
+        balance = Counter()
+        for ph in self.profile.measures:
+            period = tariff.get_period_by_date(ph.date)
+            balance[period.code] += ph.measure
+        with vcr.use_cassette('spec/fixtures/ree/201503-201504.yaml'):
+            profile = self.profile.estimate(tariff, balance)
+        expect(profile.n_hours).to(equal(self.profile.n_hours))
+
+    with context('The difference adjusting must be customitzable and checked per period'):
+        with context('If is acceptable'):
+            with it('doesn\'t have to adjust'):
+                tariff = T20DHA()
+                balance = Counter()
+
+                measures = [
+                    ProfileHour(m.date, m.measure * 1000, m.valid)
+                        for m in self.profile.measures
+                ]
+                profile = Profile(
+                    self.profile.start_date,
+                    self.profile.end_date,
+                    measures
+                )
+
+                for ph in profile.measures:
+                    period = tariff.get_period_by_date(ph.date)
+                    balance[period.code] += ph.measure
+
+                balance[period.code] -= 940
+
+                total_energy = sum(balance.values())
+                expect(total_energy).to(be_below(profile.total_consumption))
+                profile = profile.adjust(tariff, balance, 1000)
+                expect(total_energy).to(equal(profile.total_consumption - 940))
+
+                balance = Counter()
+
+                for ph in profile.measures:
+                    period = tariff.get_period_by_date(ph.date)
+                    balance[period.code] += ph.measure
+
+                balance[period.code] += 360
+
+                total_energy = sum(balance.values())
+                expect(total_energy).to(be_above(profile.total_consumption))
+                profile = profile.adjust(tariff, balance, 1000)
+                expect(total_energy).to(equal(profile.total_consumption + 360))
+
+        with context('If is not acceptable'):
+            with it('have to adjust and record the period adjusted'):
+                tariff = T20DHA()
+                balance = Counter()
+
+                for ph in self.profile.measures:
+                    period = tariff.get_period_by_date(ph.date)
+                    balance[period.code] += ph.measure
+
+                balance[period.code] += 10
+                adjusted_periods = [period.code]
+
+
+
+                total_energy = sum(balance.values())
+                expect(total_energy).to(be_above(self.profile.total_consumption))
+                profile = self.profile.adjust(tariff, balance, 1)
+                expect(total_energy).to(equal(profile.total_consumption))
+
+                adjusted_balance = Counter()
+                for ph in profile.measures:
+                    period = tariff.get_period_by_date(ph.date)
+                    adjusted_balance[period.code] += ph.measure
+
+                for period in adjusted_balance:
+                    expect(adjusted_balance[period]).to(equal(balance[period]))
+
+                expect(profile.adjusted_periods).to(
+                    contain_exactly(*adjusted_periods)
+                )
+
