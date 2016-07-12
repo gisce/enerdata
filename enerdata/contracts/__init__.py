ce51b7c1623f1d9b113227a180a527a63b9fef28
Xavi Torelló <info@xaviertorello.cat>
2016-07-12 17:36:45 +0200

    Enhacement "Avoid using wildcard (*) imports"

--- enerdata/contracts/__init__.py
+++ enerdata/contracts/__init__.py
@@ -2,5 +2,5 @@ from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 from future import *
 
-from .contract import *
-from .tariff import *
+from .contract import CONTRACT_STATES, Interval, Contract
+from .tariff import Tariff, TariffPeriod, T20A, T20DHA, T20DHS, T21A, T21DHA, T21DHS, T30A, T31A
