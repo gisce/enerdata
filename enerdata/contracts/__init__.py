1cdb7c70da8f099b12e9c0baf8f98d0deb7249ec
Xavi Torelló <info@xaviertorello.cat>
2018-02-02 14:04:04 +0100

    Add normalized_power to contracts package

--- enerdata/contracts/__init__.py
+++ enerdata/contracts/__init__.py
@@ -4,3 +4,4 @@ from future import *
 
 from .contract import CONTRACT_STATES, Interval, Contract
 from .tariff import Tariff, TariffPeriod, T20A, T20DHA, T20DHS, T21A, T21DHA, T21DHS, T30A, T31A
+from .normalized_power import *
