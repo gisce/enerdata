f332ba1f4b3a04fc475623a067d33ff6f193b091
Eduard Carreras <ecarreras@gisce.net>
2018-02-05 16:48:49 +0100

    Bump to v0.13.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.version_info < (2, 7):
 
 setup(
     name='enerdata',
-    version='0.13.1',
+    version='0.13.2',
     packages=find_packages(),
     url='http://code.gisce.net',
     license='MIT',
