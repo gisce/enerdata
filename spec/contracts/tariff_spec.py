5dfab32ecb337c711e69a376560df168fe815c4e
Xavi Torelló <info@xaviertorello.cat>
2018-02-16 14:41:17 +0100

    Add 3.0A summer weekend specification

--- spec/contracts/tariff_spec.py
+++ spec/contracts/tariff_spec.py
@@ -956,6 +956,21 @@ with description('Correct period for tariff an hour'):
             assert self.tarifa.get_period_by_date(dia + timedelta(hours=23)).code == 'P5'
             assert self.tarifa.get_period_by_date(dia + timedelta(hours=24)).code == 'P5'
 
+        with it('should have correct period on weekend summer data'):
+            dia = self.summer_weekend_day
+            assert self.tarifa.get_period_by_date(dia).code == 'P2' # points to latest hour of friday
+            assert self.tarifa.get_period_by_date(dia + timedelta(hours=1)).code == 'P6'
+            assert self.tarifa.get_period_by_date(dia + timedelta(hours=2)).code == 'P6'
+            assert self.tarifa.get_period_by_date(dia + timedelta(hours=8)).code == 'P6'
+            assert self.tarifa.get_period_by_date(dia + timedelta(hours=9)).code == 'P5'
+            assert self.tarifa.get_period_by_date(dia + timedelta(hours=10)).code == 'P5'
+            assert self.tarifa.get_period_by_date(dia + timedelta(hours=11)).code == 'P5'
+            assert self.tarifa.get_period_by_date(dia + timedelta(hours=12)).code == 'P4'
+            assert self.tarifa.get_period_by_date(dia + timedelta(hours=15)).code == 'P4'
+            assert self.tarifa.get_period_by_date(dia + timedelta(hours=16)).code == 'P5'
+            assert self.tarifa.get_period_by_date(dia + timedelta(hours=17)).code == 'P5'
+            assert self.tarifa.get_period_by_date(dia + timedelta(hours=23)).code == 'P5'
+            assert self.tarifa.get_period_by_date(dia + timedelta(hours=24)).code == 'P5'
 
     with context('3.1A'):
         with before.all:
