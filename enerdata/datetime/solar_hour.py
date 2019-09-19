ff5c14566bd51d4cb6d5b5c9a32ce19982a8bb89
puig <francescpuig7@gmail.com>
2019-09-19 18:23:13 +0200

    More secure method

--- enerdata/datetime/solar_hour.py
+++ enerdata/datetime/solar_hour.py
@@ -12,8 +12,10 @@ def convert_to_solar_hour(civil_hour=False):
     if not civil_hour:
         civil_hour = datetime.now()
     if not isinstance(civil_hour, datetime):
-        raise ValueError('% date is not a datetime' % civil_hour)
-    solar_hour = TIMEZONE.localize(civil_hour, is_dst=True)  # No daylight saving time
-    solar_hour = solar_hour.astimezone(utc)
+        raise ValueError('% datetime was expected' % civil_hour)
+    if not civil_hour.tzinfo:
+        solar_hour = TIMEZONE.localize(civil_hour, is_dst=True)  # daylight saving time
+    else:
+        solar_hour = civil_hour.astimezone(utc)
 
     return solar_hour
