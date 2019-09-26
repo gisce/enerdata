c46eb4d2644312a3172ffc356e98cafc66d8f470
puig <francescpuig7@gmail.com>
2019-09-26 09:38:31 +0200

    Imp not located datetime

--- enerdata/datetime/solar_hour.py
+++ enerdata/datetime/solar_hour.py
@@ -3,18 +3,16 @@ from enerdata.datetime.timezone import TIMEZONE
 from pytz import utc
 
 
-def convert_to_solar_hour(civil_hour=False):
+def convert_to_solar_hour(civil_hour):
     """
     Convert datetime to equivalent datetime solar hour
-    :param civil_hour: datetime
+    :param civil_hour: datetimed
     :return: datetime equivalent solar hour
     """
-    if not civil_hour:
-        civil_hour = datetime.now()
     if not isinstance(civil_hour, datetime):
         raise ValueError('datetime was expected, found {}'.format(civil_hour))
     if not civil_hour.tzinfo:
-        civil_hour = TIMEZONE.localize(civil_hour, is_dst=True)  # daylight saving time
+        raise ValueError('datetime must be located: {}'.format(civil_hour))
     solar_hour = civil_hour.astimezone(utc)
 
     return solar_hour
