68d871d3764f23a93bd40f7b1d3d1ecdb56639a9
puig <francescpuig7@gmail.com>
2020-10-27 17:18:55 +0100

    Bump to v0.24.0-rc.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if sys.version_info < (2, 7):
 
 setup(
     name='enerdata',
-    version='0.23.1',
+    version='0.24.0-rc.1',
     packages=find_packages(),
     url='http://code.gisce.net',
     license='MIT',
