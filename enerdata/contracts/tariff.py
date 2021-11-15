6f3b777d7dcdf6a9dea1ee2059ca82b245588bb1
david <dmunoz@gisce.net>
2021-11-15 15:34:31 +0100

    Fix get_holidays in TD tariffs

--- enerdata/contracts/tariff.py
+++ enerdata/contracts/tariff.py
@@ -143,7 +143,7 @@ class Tariff(object):
     def get_period_by_date(self, date_time, holidays=None, magn='te'):
         station = get_station(date_time)
         if holidays is None:
-            holidays = []
+            holidays = get_holidays(date_time.year)
         date = date_time.date()
         if (calendar.weekday(date.year, date.month, date.day) in (5, 6)
                 or date in holidays):
